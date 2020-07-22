a61b42f60af2cd897b3a05fe7be52bbbfe4e9cd0
Eng. Juan Camilo Gómez Cadavid MSc <kmilo17pet@gmail.com>
2020-07-22 17:21:58 -0500

    Update travis-texbuild.sh

--- .travis/travis-texbuild.sh
+++ .travis/travis-texbuild.sh
@@ -35,7 +35,7 @@ commit_pdfs() {
 upload_files() {
   git remote add origin-login "https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git"
   #git push --set-upstream -f origin-login "travis-$TRAVIS_BUILD_NUMBER"
-  git push --set-upstream -f origin-login master
+  git push origin-login master
   #echo "PUSHED PDFS TO BRANCH travis-$TRAVIS_BUILD_NUMBER"
   echo "PUSHED PDFS TO BRANCH master"
 }
